906b56422133fa4c923fdaa6bd6738015e95006b
Hugo FOSSE <fosse.hugo25@gmail.com>
2021-11-30 17:11:29 +0100

    Update posts and design

--- source.js
+++ source.js
@@ -7,18 +7,18 @@ let posts = Array();
 
 
 async function API_fetch(page, id) {
-   
+
     if (!page) return
 
     let url = null;
 
     if (!id) url = `https://jsonplaceholder.typicode.com/${page}`;
-    else{
+    else {
 
         url = `https://jsonplaceholder.typicode.com/${page}?userId=${id}`;
 
-    } 
-    
+    }
+
     return fetch(url)
         .then(response => response.json())
         .then(json => json).catch(error => console.error('ERROR', error))
@@ -32,11 +32,12 @@ async function API_fetch(page, id) {
         user.ouvert = false;
         let name = document.createElement("div");
         name.setAttribute('id', user.id);
+        name.setAttribute('class', "user_div");
         name.innerHTML = `<p>${user.name}</p>`;
         p_data.appendChild(name);
-        name.addEventListener("click", async function(){
-            
-            if(user.ouvert == false){
+        name.addEventListener("click", async function () {
+
+            if (user.ouvert == false) {
 
                 posts = await API_fetch("posts", index);
                 console.log(posts);
@@ -45,20 +46,21 @@ async function API_fetch(page, id) {
                 post_content.setAttribute('class', "posts_container");
                 name.appendChild(post_content);
                 posts.forEach((post) => {
-
-                    post_content.innerHTML = `<p>${post.title}</p>`;
+                    console.log(post);
                     let body_post_content = document.createElement("div");
                     body_post_content.setAttribute('class', "body_posts_container");
-                    body_post_content.innerHTML = `<p>${post.body}</p>`;
+                    body_post_content.innerHTML = `
+                    <h4>${post.title}</h4>
+                    <p>${post.body}</p>`;
                     post_content.appendChild(body_post_content);
 
                 })
 
-                    
-                
+
+
                 user.ouvert = true;
 
-            }else{
+            } else {
                 name.removeChild(document.getElementById(`user${user.id}_container`))
                 console.log("else");
                 user.ouvert = false;
