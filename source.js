8ec84b5419aaf5b663abc4153ea61a7b07a5e44d
Hugo FOSSE <fosse.hugo25@gmail.com>
2021-11-30 14:34:01 +0100

    maj

--- source.js
+++ source.js
@@ -1,3 +1,45 @@
-const promise = fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(json => console.log(json));
+let users = Array();
+const p_data = document.getElementById("content")
+console.log(p_data);
+
+let posts = Array();
+
+
+
+async function API_fetch(page, id) {
+    if (!page) return
+    const url = null;
+    if (!id) url = `https://jsonplaceholder.typicode.com/${page}`;
+    else{
+
+        url = `https://jsonplaceholder.typicode.com/${page}?userId=${id}`;
+
+    } 
+    
+    return fetch(url)
+        .then(response => response.json())
+        .then(json => json).catch(error => console.error('ERROR', error))
+}
+
+(async () => {
+    users = await API_fetch("users");
+    console.log(users)
+    users.forEach((user, index) => {
+        let name = document.createElement("div");
+        name.setAttribute('id', user.id);
+        name.innerHTML = `<p>${user.name}</p>`;
+        p_data.appendChild(name);
+        name.addEventListener("click", function(){
+
+           posts =  API_fetch("posts", index);
+           let post_content = document.createElement("div");
+           post_content.setAttribute('id', user.id);
+           name.appendChild(post_content);
+           post_content.innerHTML = `<p>${user.title}`;
+
+        })
+    })
+})();
+
+
+
